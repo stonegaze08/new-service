8095b344be9a20c9b2002e4ec22fdd2b43b4a675
zulna10 <zulkarnain10062001@gmail.com>
2024-05-05 15:06:54 -0700

    update

--- components/border.jsx
+++ components/border.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const CustomImage = ({ src, alt, borderRadius, margin }) => {
-  const style = {
+const ImageWithText = ({ src, alt, text, marginRight, marginTop, borderRadius, fontSize, fontWeight }) => {
+  const imageStyle = {
+    marginRight: marginRight || '10px',
     borderRadius: borderRadius || '50%',
-    margin: margin || '0',
+    marginTop: marginTop || '18px'
+  };
+
+  const textStyle = {
+    fontSize: fontSize || '50px', 
+    fontWeight: fontWeight || '600'
   };
 
   return (
-    <img src={src} alt={alt} style={style} width={'75'} height={'75'} />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={src} alt={alt} style={imageStyle} width={'85'} height={'85'} />
+      <span style={textStyle}>{text}</span>
+    </div>
   );
 };
 
-export default CustomImage;
-
+export default ImageWithText;
