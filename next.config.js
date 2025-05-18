6c74d2d01c4a3a4a4e7629a373fe46052718b5b7
harditakurniawan <hardita.kurniawan@gmail.com>
2025-05-18 11:46:25 +0700

    add new sub folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    rehypePlugins: [require('rehype-raw')],
+  },
 })
 
 module.exports = withNextra()
